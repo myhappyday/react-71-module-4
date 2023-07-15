04075f2da0f983a6a31384dfbf1058beaed136b9
Maryna Khmarska <111706884+myhappyday@users.noreply.github.com>
2023-07-15 22:48:50 +0300

    Module 4 Lesson 7

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { Routes, Route } from 'react-router-dom';
+import AppBar from './components/AppBar/AppBar';
+import SignupForm from './components/SignupForm/SignupForm';
+import ColorPicker from './components/ColorPicker/ColorPicker';
+import Counter from './components/Counter/Counter';
+import Clock from './components/Clock/Clock';
+import PokemonView from './views/PokemonView';
+
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
+
+const containerStyles = {
+  maxWidth: 1170,
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  paddingLeft: 15,
+  paddingRight: 15,
+};
+
+export default function App() {
+  return (
+    <div style={containerStyles}>
+      <AppBar />
+
+      <Routes>
+        <Route path="/signup" element={<SignupForm />} />
+        <Route
+          path="/colorpicker"
+          element={<ColorPicker options={colorPickerOptions} />}
+        />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/clock" element={<Clock />} />
+        <Route path="/pokemon" element={<PokemonView />} />
+        {/* <Route path="/signup">
+          <SignupForm />
+        </Route>
+
+        <Route path="/colorpicker">
+          <ColorPicker options={colorPickerOptions} />
+        </Route>
+
+        <Route path="/counter">
+          <Counter />
+        </Route>
+
+        <Route path="/clock">
+          <Clock />
+        </Route>
+
+        <Route path="/pokemon">
+          <PokemonView />
+        </Route> */}
+      </Routes>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import AuthProvider from './contexts/auth/Provider';
+import 'modern-normalize/modern-normalize.css';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <AuthProvider>
+//         <App />
+//       </AuthProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from 'components/App';
+// import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
   <React.StrictMode>
-    <App />
+    <BrowserRouter
+    // basename="/react-71-module-4"
+    >
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
