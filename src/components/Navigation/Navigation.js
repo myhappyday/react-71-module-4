d2b1de39a2e082cb351be6b1a43cb97c3d7ef3c4
Maryna Khmarska <111706884+myhappyday@users.noreply.github.com>
2023-08-07 17:11:29 +0300

    Update styles Navigation active

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,64 +8,60 @@ const Navigation = () => (
     <NavLink
       to="/signup"
       className={styles.link}
-      // activeClassName={styles.activeLink}
+      activeClassName={styles.linkActive}
     >
       Форма
     </NavLink>
     <NavLink
       to="/colorpicker"
       className={styles.link}
-      // activeClassName={styles.activeLink}
+      activeClassName={styles.linkActive}
     >
       Колорпікер
     </NavLink>
     <NavLink
       to="/counter"
       className={styles.link}
-      // activeClassName={styles.activeLink}
+      activeClassName={styles.linkActive}
     >
       Лічильник
     </NavLink>
     <NavLink
       to="/clock"
       className={styles.link}
-      // activeClassName={styles.activeLink}
+      activeClassName={styles.linkActive}
     >
       Годинник
     </NavLink>
     <NavLink
       to="/pokemon"
       className={styles.link}
-      // activeClassName={styles.activeLink}
+      activeClassName={styles.linkActive}
     >
       Покемони з хуками
     </NavLink>
     <NavLink
       to="/skip-first-render"
       className={styles.link}
-      // activeClassName={styles.activeLink}
+      activeClassName={styles.linkActive}
     >
       Пропуск першого рендера
     </NavLink>
     <NavLink
       to="/counter-4.2"
       className={styles.link}
-      // activeClassName={styles.activeLink}
+      activeClassName={styles.linkActive}
     >
       useReducer
     </NavLink>
     <NavLink
       to="/notes"
       className={styles.link}
-      // activeClassName={styles.activeLink}
+      activeClassName={styles.linkActive}
     >
       useMemo
     </NavLink>
-    <NavLink
-      to="/news"
-      className={styles.link}
-      // activeStyle={styles.activeLink}
-    >
+    <NavLink to="/news" className={styles.link} activeStyle={styles.linkActive}>
       Новини
     </NavLink>
   </nav>
